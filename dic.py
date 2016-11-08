6475cbbfccdf4978c3bd3d59612cbfaf503798a3
grance1 <2397580855@qq.com>
2016-11-08 13:51:31 +0800

    this is a dic file

---
+++ dic.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python2.7
+d = {'Adam':95,
+     'Lisa':85,
+     'Bart':59}
+print d
+print "Adam:", d['Adam']
+print "Lisa:",d['Lisa']
+print "Bart:",d['Bart']
+print d['Adam']
+print
+print
+print d
+print len(d)
+print d['Adam']
+print d.get('Paul')
+d = {'Adam':95,
+     'Lisa':85,
+     'Bart':59,
+     'Paul':75}
+print d
+print len(d)
+print d.get('Bart')
