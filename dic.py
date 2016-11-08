f14e57fc3a44206c239598f1bdaabfae9349a3c1
grance1 <2397580855@qq.com>
2016-11-08 14:16:09 +0800

    modify this file

--- dic.py
+++ dic.py
@@ -20,3 +20,31 @@ d = {'Adam':95,
 print d
 print len(d)
 print d.get('Bart')
+dic = {95:'Adam',
+       85:'Lisa',
+       59:'Bart'}
+print dic[95]
+
+d = {'Adam':95,
+     'Lisa':85,
+     'Bart':59}
+print d
+d['Paul'] = 72
+print d
+d['Bart']=60
+print d
+
+d = {95:'Adam',
+     85:'Lisa',
+     59:'Bart'}
+d[72] = 'Paul'
+print d
+for key in d:
+    print key
+
+d = {'Adam':95,
+     'Lisa':85,
+     'Bart':59}
+for key in d:
+    print key,":",d[key]
+
