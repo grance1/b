9eaf97d3d08d8178640b52d5a33de0105dfced64
grance1 <2397580855@qq.com>
2016-11-27 16:31:55 +0800

    this is a class file

---
+++ class.py
@@ -0,0 +1,90 @@
+#!/usr/bin/python2.7
+class Student(object):
+    def __init__(self,name,score):
+        self.name = name
+        self.score = score
+bart = Student('Bart Simpson',59)
+print bart.name
+print bart.score
+
+def print_score(std):
+    print('%s:%s'%(std.name,std.score))
+
+print_score(bart)
+class Student(object):
+    def __init__(self,name,score):
+        self.name = name
+        self.score = score
+    def print_score(self):
+        print('%s:%s' % (self.name, self.score))
+    def get_grade(self):
+        if self.score >= 90:
+            return 'A'
+        if self.score >= 60:
+            return 'B'
+        else:
+            return 'C'
+bart = Student('Bart Simpson', 59)
+print bart.print_score()
+print bart.get_grade()
+
+
+bart = Student('Bart Simpson',98)
+print bart.score
+bart.score = 59
+print bart.score
+
+class Student(object):
+    def __init__(self,name,score):
+        self.__name = name
+        self.__score = score
+    def print_score(self):
+        print('%s: %s' % (self.__name, self.__score))
+bart = Student('Bart Simpson', 98)
+print  bart.__name
+class Student(object):
+    pass
+
+bart = Student()
+print bart
+print Student
+bart.name = 'Bart Simpson'
+print bart.name
+bart = Student('Bart Simpson', 59)
+print bart.print_score()
+print bart.get_grade()
+class Dog(Animal):
+
+    def run(self):
+        print('Dog is running...')
+
+    def eat(self):
+        print('Eating meat...')
+class Dog(Animal):
+
+    def run(self):
+        print('Dog is running...')
+
+class Cat(Animal):
+
+    def run(self):
+        print('Cat is running...')
+dog = Dog()
+print dog.run()
+class Animal(object):
+    def run(self):
+        print('Animal is running...')
+class Dog(Animal):
+    pass
+
+class Cat(Animal):
+    pass
+dog = Dog()
+print dog.run()
+bart = Student('Bart Simpson',59)
+lisa = Student('Lisa Simpson',87)
+print bart.print_score()
+print lisa.print_score()
+
+
+
